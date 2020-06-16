49710675b79bad25e8f97d00a70fe288f496ca71
lee <ce.li@thoughtworks.com>
2020-06-16 22:33:49 +0800

    practice 2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
+@Scope("prototype")
+//@RequestScope
 public class GreetingController {
 
     private final GreetingService greetingService;
@@ -12,6 +16,10 @@ public class GreetingController {
     @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
+    }
+
+    public GreetingService getGreetingService() {
+        return greetingService;
     }
 
     @GetMapping("/greet")
--- src/test/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/demospringioccontainer/DemoSpringIocContainerApplicationTests.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoSpringIocContainerApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		GreetingController greetingController1 = applicationContext.getBean(GreetingController.class);
+		GreetingController greetingController2 = applicationContext.getBean(GreetingController.class);
+		assertEquals(greetingController1.getGreetingService(), greetingController2.getGreetingService());
 	}
 
 }
